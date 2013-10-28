69b51ed6d8b511e07b787d18165fea31b749751e
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-28 14:18:44 -0500

    bcrypt/profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ActionController::Base
 
   def new
+    @user = User.new
   end
 
   def create
-    @user = User.create(params[:user])
-    redirect_to @user
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Yeeeee!!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  #  def user_params
+  #   params.require(:user).permit(:name, :email, :password)
+  # end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  has_secure_password
+  validates :email, :password, :username, :presence => { :on => :create }
   attr_accessible :email, :password, :username
 
   has_many :posts
