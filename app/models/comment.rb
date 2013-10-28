241cbde8917f010380827150fb173dee75b21ee8
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-28 10:41:14 -0500

    associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   attr_accessible :comment_text
+  belongs_to :user
+  belongs_to :post
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :username
+  attr_accessible :email, :password_digest, :username
+
+  has_many :posts
+  has_many :comments
 end
