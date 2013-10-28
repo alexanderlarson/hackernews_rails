a68fddb55e037318f43825498b8dae52a822c76e
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-28 16:33:18 -0500

    login/sessions working

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,12 +3,14 @@ class UserSessionsController < ApplicationController
   end
   
   def create
+    p params
     @user = User.find_by_email(params[:user][:email])
+    session[:id] = @user.id
     redirect_to posts_path
   end
 
   def destroy
-    session[:user_id] = nil
+    session[:id] = nil
     redirect_to posts_path
   end
 end
